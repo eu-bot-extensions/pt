9be98d8f4d668e835c383770d40563d2bd9ab1b0
SimoneDavi55 <109947200+SimoneDavi55@users.noreply.github.com>
2023-05-07 18:40:45 +0200

    Aggiorna main.ts

--- main.ts
+++ main.ts
@@ -615,12 +615,30 @@ namespace eurate {
     }
 
     /**
+    * Return the value of distance in cm
+    * @param trig tigger pin
+    * @param echo echo pin
+    */
+    //% weight=100
+    //% blockId=sonar_ping block="ping trig %trigpin|echo %echopin"
+    export function UsSensor(trigpin:DigitalPin, echopin: DigitalPin) : number
+    {
+        pins.digitalWritePin(trigpin, 0)
+        control.waitMicros(2)
+        pins.digitalWritePin(trigpin, 1)
+        control.waitMicros(10)
+        pins.digitalWritePin(trigpin, 0)
+        return Math.idiv(pins.pulseIn(echopin, PulseValue.High), 58)
+    }
+
+    /**
     * Send a ping and get the echo time (in microseconds) as a result
     * @param trig tigger pin
     * @param echo echo pin
     * @param unit desired conversion unit
     * @param maxCmDistance maximum distance in centimeters (default is 500)
     */
+    //% weight=1
     //% blockId=sonar_ping block="ping trig %trig|echo %echo|unit %unit"
     export function ping(trig: DigitalPin, echo: DigitalPin, unit: PingUnit, maxCmDistance = 500): number {
         // send pulse
